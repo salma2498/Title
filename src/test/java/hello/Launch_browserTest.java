dae77c60c004be6ffbe4ee9d1d267166ad589494
salma2498 <salmasalu487@gmail.com>
2023-11-30 11:50:28 +0530

    first commit

--- src/test/java/hello/Launch_browserTest.java
+++ src/test/java/hello/Launch_browserTest.java
@@ -4,19 +4,31 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Launch_browserTest {
+	WebDriver driver;
 	@Test
 	public void test1()
 	{
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.get("https://www.amazon.com/");
-		String title = driver.getTitle();
-		System.out.println(title);
-		driver.quit();
+		String browser = System.getProperty("Browser");
+		String URL = System.getProperty("Url");
+
+		if(browser.equalsIgnoreCase("Chrome"))
+		{
+			driver=new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			driver=new FirefoxDriver();
+		}
+		else
+		{
+			driver=new ChromeDriver();
+		}
+		driver.get(URL);
+		
 	}
 
 }
